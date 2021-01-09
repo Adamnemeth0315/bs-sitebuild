2be27b78b5341222df5d773585b7230b23100acb
Adam <adam.nemeth.0315@gmail.com>
2021-01-09 15:43:13 +0100

    Responsive az oldal még a navbar van hátra.

--- asstets/js/main.js
+++ asstets/js/main.js
@@ -3,3 +3,17 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+const navbar = document.querySelector('.nav');
+const sticky = navbar.offsetTop;
+
+function stickyNavBarFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        navbar.classList.add('bg-light')
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+window.addEventListener('scroll', stickyNavBarFunction);
