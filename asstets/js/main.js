e00b6f5ac837e7a44aeb74256a1f4d209e137b6f
Adam <adam.nemeth.0315@gmail.com>
2021-01-11 11:20:31 +0100

    Már csak a navbar scroll amit szépíteni kell

--- asstets/js/main.js
+++ asstets/js/main.js
@@ -6,9 +6,10 @@ $(function () {
 
 const navbar = document.querySelector('.nav');
 const sticky = navbar.offsetTop;
+const link = document.querySelectorAll('.nav-link');
 
 function stickyNavBarFunction() {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset > sticky) {
         navbar.classList.add("sticky");
         navbar.classList.add("bg-light");
     } else {
@@ -16,4 +17,25 @@ function stickyNavBarFunction() {
     }
 }
 
+function stickyNavBarFunction() {
+    if (window.pageYOffset = sticky) {
+        navbar.classList.remove("sticky");
+    } else {
+        navbar.classList.add("sticky");
+    }
+}
+
 window.addEventListener('scroll', stickyNavBarFunction);
+
+
+$("navbar").find("link").click(function (e) {
+    e.preventDefault();
+    let section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    }, 1000, 'easeInOutExpo');
+});
+
+let scroll = SmoothScroll('a[href*="#"', {
+    speed: 300
+});
